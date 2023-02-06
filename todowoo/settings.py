62f5720ebbf7b5c9b475be62ba9eb74b5065979f
Nikita Kovall <goodn7722@gmail.com>
2023-02-06 23:36:09 +0200

    hostasd

--- todowoo/settings.py
+++ todowoo/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+!!+$@$^pv#iiz!&fs51=o@83)745xad7*eoc)g!a8fff1ht)a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['kovall.pythonanywhere.com']
 
